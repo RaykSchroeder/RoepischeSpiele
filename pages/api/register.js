63312d3b87a0eec941b02ffb8c820b1fa280a510
RaykSchroeder <87695031+RaykSchroeder@users.noreply.github.com>
2025-10-19 19:38:33 +0200

    Update register.js

--- pages/api/register.js
+++ pages/api/register.js
@@ -9,17 +9,16 @@ export default async function handler(req, res) {
   if (req.method !== "POST")
     return res.status(405).json({ error: "Nur POST erlaubt" });
 
-  const { name, address, phone, email } = req.body;
+  const { name, address, phone, email, how_found, contact_person } = req.body;
 
   if (!name || !email)
     return res.status(400).json({ error: "Name und E-Mail sind erforderlich." });
 
   const { error } = await supabase.from("registrations").insert([
-    { name, address, phone, email },
+    { name, address, phone, email, how_found, contact_person },
   ]);
 
   if (error) return res.status(500).json({ error: error.message });
 
   res.status(200).json({ message: "Registrierung erfolgreich!" });
 }
-
